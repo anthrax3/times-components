6a339f0820ff47620bb5c0aed2fc645720d5e939
James Miller <jimhunty@gmail.com>
2018-02-21 10:47:27 +0000

    test: Adding video tests to web

--- packages/brightcove-video/__tests__/web/brightcove.web.test.js
+++ packages/brightcove-video/__tests__/web/brightcove.web.test.js
@@ -1,5 +1,7 @@
 import brightcoverPlayerWeb from "../brightcove-player.web";
+import brightcoveVideo from "../brightcove-video";
 
 describe("Brightcove tests on web", () => {
   brightcoverPlayerWeb();
+  brightcoveVideo();
 });
