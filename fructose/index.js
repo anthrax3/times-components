80c4c59b7a8dee45ab7658bb5b34c7a40f801817
L0wry <matthewlowry@hotmail.co.uk>
2018-02-22 16:54:37 +0000

    fix: amending fonts

--- fructose/index.js
+++ fructose/index.js
@@ -8,13 +8,13 @@ AppRegistry.registerComponent("storybooknative", () => Fructose(loadStories));
 Expo.registerRootComponent(Fructose(loadStories));
 
 Font.loadAsync({
-      "GillSansMTStd-Medium": require(`${__dirname}/../dist/public/fonts/GillSansMTStd-Medium.ttf`),
-      "TimesDigitalW04-Regular": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04-Regular.ttf`),
-      "TimesDigitalW04-RegularSC": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04-RegularSC.ttf`),
-      "TimesDigitalW04": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04.ttf`),
-      "TimesDigitalW04_bold": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04_bold.ttf`),
-      "TimesDigitalW04_italic": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04_italic.ttf`),
-      "TimesModern-Bold": require(`${__dirname}/../dist/public/fonts/TimesModern-Bold.ttf`),
-      "TimesModern-Regular": require(`${__dirname}/../dist/public/fonts/TimesModern-Regular.ttf`),
+      "GillSansMTStd-Medium": require(`../dist/public/fonts/GillSansMTStd-Medium.ttf`),
+      "TimesDigitalW04-Regular": require(`../dist/public/fonts/TimesDigitalW04-Regular.ttf`),
+      "TimesDigitalW04-RegularSC": require(`../dist/public/fonts/TimesDigitalW04-RegularSC.ttf`),
+      "TimesDigitalW04": require(`../dist/public/fonts/TimesDigitalW04.ttf`),
+      "TimesDigitalW04_bold": require(`../dist/public/fonts/TimesDigitalW04_bold.ttf`),
+      "TimesDigitalW04_italic": require(`../dist/public/fonts/TimesDigitalW04_italic.ttf`),
+      "TimesModern-Bold": require(`../dist/public/fonts/TimesModern-Bold.ttf`),
+      "TimesModern-Regular": require(`../dist/public/fonts/TimesModern-Regular.ttf`),
 });
 
