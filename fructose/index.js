56d5177832f76bfb31a5112245d5e57b0f2ffeca
L0wry <matthewlowry@hotmail.co.uk>
2018-02-22 16:48:50 +0000

    feat: Adding expo to times components

--- fructose/index.js
+++ fructose/index.js
@@ -1,6 +1,20 @@
 import { AppRegistry } from "react-native";
+import Expo, { Font } from "expo";
 import Fructose from "@times-components/fructose";
 // eslint-disable-next-line import/no-unresolved
 import { loadStories } from "./components";
 
 AppRegistry.registerComponent("storybooknative", () => Fructose(loadStories));
+Expo.registerRootComponent(Fructose(loadStories));
+
+Font.loadAsync({
+      "GillSansMTStd-Medium": require(`${__dirname}/../dist/public/fonts/GillSansMTStd-Medium.ttf`),
+      "TimesDigitalW04-Regular": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04-Regular.ttf`),
+      "TimesDigitalW04-RegularSC": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04-RegularSC.ttf`),
+      "TimesDigitalW04": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04.ttf`),
+      "TimesDigitalW04_bold": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04_bold.ttf`),
+      "TimesDigitalW04_italic": require(`${__dirname}/../dist/public/fonts/TimesDigitalW04_italic.ttf`),
+      "TimesModern-Bold": require(`${__dirname}/../dist/public/fonts/TimesModern-Bold.ttf`),
+      "TimesModern-Regular": require(`${__dirname}/../dist/public/fonts/TimesModern-Regular.ttf`),
+});
+
--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import { AppRegistry } from "react-native";
-import Expo from 'expo';
-
-import Fructose from "@times-components/fructose";
-// eslint-disable-next-line import/no-unresolved
-import { loadStories } from "./fructose/components";
-
-
-
-AppRegistry.registerComponent("storybooknative", () => Fructose(loadStories));
-Expo.registerRootComponent(Fructose(loadStories));
+import ExpoApp from "./fructose/index"
