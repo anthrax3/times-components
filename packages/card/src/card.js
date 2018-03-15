70f51bc34cc9a32cf6a0c4ac2b57b863953835e7
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-03-15 14:47:20 +0000

    fix: updates storybook

--- packages/card/src/card.js
+++ packages/card/src/card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import Image from "@times-components/image";
 import { Animations } from "@times-components/styleguide";
 import { cardPropTypes, cardDefaultProps } from "./card-proptypes";
