35a39a70b726c4def440769156a9440083999064
L0wry <matthewlowry@hotmail.co.uk>
2018-04-16 14:05:18 +0100

    chore: bumping dextrose, changing scripts

--- lib/visual_snapshots/run-ios-dextrose.sh
+++ lib/visual_snapshots/run-ios-dextrose.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 xcrun simctl boot 'iPhone 7'
-./node_modules/.bin/lerna run dextrose-clean
-./node_modules/.bin/lerna run dextrose-stories --since
+./node_modules/.bin/lerna run babel . --out-dir lib
+./node_modules/.bin/lerna run dextrose-clean --since
+./node_modules/.bin/lerna run generate-showcase-files --since
 npm run fetch-fonts
 ./node_modules/.bin/rnstl --searchDir ./packages --pattern '**/*/*.stories!(.web).dextrose.tmp.js' --outputFile ./fructose/components.js
 ./node_modules/.bin/react-native bundle --platform ios --dev false --reset-cache --entry-file fructose/index.js --bundle-output ios/main.jsbundle
